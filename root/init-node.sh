128e6a26c6ef36a85076ad1cb4fbb8578eda12f8
Jean-Emrick <gestionjerioux@gmail.com>
2022-01-19 21:08:32 -0500

    correct the binary's path mistake for the service file

--- root/init-node.sh
+++ root/init-node.sh
@@ -134,7 +134,7 @@ Description=$serviceName service
 After=network-online.target
 [Service]
 User=$newUserCrypto
-ExecStart=/root/go/bin/$DAEMON start
+ExecStart=/var/lib/$newUserCrypto/go/bin/$DAEMON start
 Restart=always
 RestartSec=3
 LimitNOFILE=4096
