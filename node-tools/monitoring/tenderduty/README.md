62905c09c3f69607ee1a7046012f24b39b935ebe
Jean-Emrick <gestionjerioux@gmail.com>
2022-02-07 12:01:33 -0500

    add service-duty

--- node-tools/monitoring/tenderduty/README.md
+++ node-tools/monitoring/tenderduty/README.md
@@ -1,6 +1,13 @@
-And start this script:
+Start this script as the chain user (or any user that has access to GO):
 ```
 wget  https://raw.githubusercontent.com/stakefrites/scripts/main/node-tools/monitoring/tenderduty/tenderduty.sh \
 && chmod +x tenderduty.sh \
 && ./tenderduty.sh
+```
+
+Then start this script as root:
+```
+wget  https://raw.githubusercontent.com/stakefrites/scripts/main/node-tools/monitoring/tenderduty/service-duty.sh \
+&& chmod +x service-duty.sh \
+&& ./service-duty.sh
 ```
---
+++ node-tools/monitoring/tenderduty/service-duty.sh
@@ -0,0 +1,26 @@
+read -p "What is the valcons address? (y/n) :" address
+
+cat << EOF > /etc/systemd/system/tenderduty.service
+[Unit]
+Description=Tenderduty
+After=network-online.target
+
+[Service]
+User=root
+TimeoutStartSec=0
+CPUWeight=95
+IOWeight=95
+ExecStart=tenderduty -c $address -p 91f9cbd6ff6d4c00d06c2fc47ca00a5f -u http://127.0.0.1:26657 -threshold 5
+Restart=always
+RestartSec=2
+LimitNOFILE=800000
+KillSignal=SIGTERM
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable tenderduty
+systemctl start tenderduty
+systemctl status tenderduty
