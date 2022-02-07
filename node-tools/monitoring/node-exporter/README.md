0d1fe491ee347d84501c6fab92234402b880d54b
Jean-Emrick <gestionjerioux@gmail.com>
2022-02-06 20:01:18 -0500

    add node exporter

---
+++ node-tools/monitoring/node-exporter/README.md
@@ -0,0 +1,6 @@
+And start this script:
+```
+wget  https://raw.githubusercontent.com/stakefrites/scripts/main/node-tools/monitoring/node-exporter/node-exporter.sh \
+&& chmod +x node-exporter.sh \
+&& ./node-exporter.sh
+```
---
+++ node-tools/monitoring/node-exporter/node-exporter.sh
@@ -0,0 +1,27 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz
+mv node_exporter-1.3.1.linux-amd64/node_exporter /usr/bin/
+rm node_exporter-1.3.1.linux-amd64.tar.gz
+rm -rf node_exporter-1.3.1.linux-amd64
+cat << EOF > /etc/systemd/system/node-exporter.service
+[Unit]
+Description=Node Exporter
+After=network-online.target
+User=root
+TimeoutStartSec=0
+CPUWeight=95
+IOWeight=95
+ExecStart=node_exporter
+Restart=always
+RestartSec=2
+LimitNOFILE=800000
+KillSignal=SIGTERM
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable node-exporter
+systemctl start node-exporter
+systemctl status node-exporter
