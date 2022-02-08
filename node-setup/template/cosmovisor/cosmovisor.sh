482eb0f90d9ba34064ab9e195984856ab35fa0b8
Jean-Emrick <gestionjerioux@gmail.com>
2022-02-07 19:55:17 -0500

    add cosmovisor script2

--- node-setup/template/cosmovisor/cosmovisor.sh
+++ node-setup/template/cosmovisor/cosmovisor.sh
@@ -1,7 +1,7 @@
 go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@latest
-line read -p "What is the chain daemon name (binary ex: chihuahuad/lumd/akash):" DAEMON_NAME
-line read -p "What is the chain home config folder (ex: chihuahua):" USER
-line read -p "What is the chain home config folder (ex: .lumd):" HOME_CONFIG
+read -p "What is the chain daemon name (binary ex: chihuahuad/lumd/akash):" DAEMON_NAME
+read -p "What is the chain home config folder (ex: chihuahua):" USER
+read -p "What is the chain home config folder (ex: .lumd):" HOME_CONFIG
 mkdir -p /var/lib/$USER/$HOME_CONFIG/cosmovisor/genesis/bin
 mkdir -p /var/lib/$USER/$HOME_CONFIG/cosmovisor/upgrades
 cp /var/lib/$USER/go/bin/$DAEMON_NAME /var/lib/$USER/$HOME_CONFIG/cosmovisor/genesis/bin/
