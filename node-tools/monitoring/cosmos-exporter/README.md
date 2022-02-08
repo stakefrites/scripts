c94134e8d50cbf810a7a1026266ebe288338fee6
Jean-Emrick <gestionjerioux@gmail.com>
2022-02-07 22:07:34 -0500

    update cosmos-exporter script

---
+++ node-tools/monitoring/cosmos-exporter/README.md
@@ -0,0 +1,6 @@
+And start this script:
+```
+wget  https://raw.githubusercontent.com/stakefrites/scripts/main/node-tools/monitoring/cosmos-exporter/cosmos-exporter.sh \
+&& chmod +x cosmos-exporter.sh \
+&& ./cosmos-exporter.sh
+```
--- node-tools/monitoring/cosmos-exporter/cosmos-exporter.sh
+++ node-tools/monitoring/cosmos-exporter/cosmos-exporter.sh
@@ -1,11 +1,27 @@
 wget https://github.com/solarlabsteam/cosmos-exporter/releases/download/v0.2.2/cosmos-exporter_0.2.2_Linux_x86_64.tar.gz \ 
-&& tar xvfz cosmos-exporter-0.2.2-Linux-x86_64.tar.gz \
-&& sudo cp ./cosmos-exporter /usr/bin \
-&& sudo nano /etc/systemd/system/cosmos-exporter.service
+tar xvfz cosmos-exporter-0.2.2-Linux-x86_64.tar.gz \
+sudo cp ./cosmos-exporter /usr/bin \
+cat << EOF > /etc/systemd/system/cosmos-exporter.service
+[Unit]
+Description=Cosmos Exporter
+After=network-online.target
 
+[Service]
+User=<username>
+TimeoutStartSec=0
+CPUWeight=95
+IOWeight=95
+ExecStart=cosmos-exporter
+Restart=always
+RestartSec=2
+LimitNOFILE=800000
+KillSignal=SIGTERM
 
---denom ulum --denom-coefficient 1000000 --bech-prefix lum --log-level debug
+[Install]
+WantedBy=multi-user.target
+EOF
 
---denom uakt --denom-coefficient 1000000 --bech-prefix akash --log-level debug
-
---denom uhuahua --denom-coefficient 1000000 --bech-prefix chihuahua --log-level debug
+systemctl daemon-reload
+systemctl enable cosmos-exporter
+systemctl start cosmos-exporter
+systemctl status cosmos-exporter
