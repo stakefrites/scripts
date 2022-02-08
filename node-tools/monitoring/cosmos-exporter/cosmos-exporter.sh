2cfc017ef3e9a3c79ce6cd008992a55a8ceed9f8
Jean-Emrick <gestionjerioux@gmail.com>
2022-02-07 22:10:32 -0500

    update service file

--- node-tools/monitoring/cosmos-exporter/cosmos-exporter.sh
+++ node-tools/monitoring/cosmos-exporter/cosmos-exporter.sh
@@ -1,3 +1,5 @@
+read -p "What is this coin's denom? (ex: uatom):" DENOM
+read -p "What is the bech prefix (ex: atom):" PREFIX
 wget https://github.com/solarlabsteam/cosmos-exporter/releases/download/v0.2.2/cosmos-exporter_0.2.2_Linux_x86_64.tar.gz \ 
 tar xvfz cosmos-exporter-0.2.2-Linux-x86_64.tar.gz \
 sudo cp ./cosmos-exporter /usr/bin \
@@ -11,7 +13,7 @@ User=<username>
 TimeoutStartSec=0
 CPUWeight=95
 IOWeight=95
-ExecStart=cosmos-exporter
+ExecStart=cosmos-exporter --denom $DENOM --denom-coefficient 1000000 --bech-prefix $PREFIX
 Restart=always
 RestartSec=2
 LimitNOFILE=800000
